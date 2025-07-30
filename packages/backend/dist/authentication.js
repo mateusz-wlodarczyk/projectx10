6db8f711e0434ef6c296caba090769f9f12f678b
matek <mateusz-wlodarczyk@o2.pl>
2025-07-30 22:47:25 +0200

    deleted folder dist

--- packages/backend/dist/authentication.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.expressAuthentication = expressAuthentication;
-function expressAuthentication(request, securityName, scopes) {
-    // For now, we'll just return a promise that resolves
-    // You can implement actual authentication logic here later
-    return Promise.resolve({});
-}
